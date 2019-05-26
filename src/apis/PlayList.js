0863809329144684e8b4ff71a6f5b37b2f337cea
Kazi Tihum <kazitihum@gmail.com>
2019-05-26 13:39:34 +0000

    Homepage List

--- src/apis/PlayList.js
+++ src/apis/PlayList.js
@@ -5,7 +5,7 @@ export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3/',
     params: {
         part: 'snippet',
-        maxResults: 1,
+        maxResults: 3,
         key: KEY,
         playlistId: 'PLq71IJk8mCV4-DqsZ7W6zRS7uzKOmmT8j'
     }
--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,37 +1,55 @@
 import React, { Component } from 'react';
+import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Fab from '@material-ui/core/Fab';
+import PlayArrow from '@material-ui/icons/PlayArrow';
+
 import PlayList from '../../apis/PlayList';
+import './home.css';
 
 export default class Home extends Component {
-  constructor(props) {
-  super(props);
-    this.state = {
-      video: [],
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            video: [],
+        };
+    }
 
-  componentDidMount() {
-  this.VideoList();
-  }
+    componentDidMount() {
+        this.VideoList();
+    }
 
-  VideoList() {
-    PlayList.get('/playlistItems')
-      .then(response => {
-        this.setState({ video: response.data.items });
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    VideoList() {
+        PlayList.get('/playlistItems')
+            .then(response => {
+                this.setState({ video: response.data.items });
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
+    }
 
-  render() {
-    return (
-      <div id="layout-content" className="layout-content-wrapper">
-        <div className="panel-list">
-          { this.state.video.map(item =>
-              <h1 key={item.id}>{item.snippet.title}</h1>
-          )}
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <Container maxWidth="sm">
+                <Box py="16px">
+                    <Grid container cols={2} spacing={2}>
+                        {this.state.video.map(item =>
+                            <Grid item xs={6} key={item.id}>
+                                <Box position="relative" borderRadius={8} className="overflow shadow" mb={1}>
+                                    <img src={item.snippet.thumbnails.high.url} alt={item.snippet.title} width="100%" height="100%" />
+                                    <Fab color="primary" size="small" aria-label="Play" className="floatingFab">
+                                        <PlayArrow />
+                                    </Fab>
+                                </Box>
+                                <Typography variant="subtitle2" component="h1" m={0}>{item.snippet.title}</Typography>
+                            </Grid>
+                        )}
+                    </Grid>
+                </Box>
+            </Container>
+        );
+    }
 }
