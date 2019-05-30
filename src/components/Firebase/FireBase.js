bd0a168deb3eccaefa29988dc728654cf9378ae6
Kazi Tihum <kazitihum@gmail.com>
2019-05-30 18:52:57 +0000

    Landing page modification

--- src/components/Firebase/FireBase.js
+++ src/components/Firebase/FireBase.js
@@ -2,18 +2,28 @@ import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
 
-const configProd = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// const configProd = {
+//   apiKey: process.env.REACT_APP_API_KEY,
+//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+//   databaseURL: process.env.REACT_APP_DATABASE_URL,
+//   projectId: process.env.REACT_APP_PROJECT_ID,
+//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+// };
+
+const configDev = {
+  apiKey: "AIzaSyCY0y2xaJ8LJ9p31ksIeU1-Hk6_x7j84uQ",
+  authDomain: "soundcade-bc6a0.firebaseapp.com",
+  databaseURL: "https://soundcade-bc6a0.firebaseio.com",
+  projectId: "soundcade-bc6a0",
+  storageBucket: "soundcade-bc6a0.appspot.com",
+  messagingSenderId: "876132674041",
+  appId: "1:876132674041:web:6b0e3bc34673774c"
 };
 
 class Firebase {
   constructor() {
-    app.initializeApp(configProd);
+    app.initializeApp(configDev);
 
     /* Helper */
 
--- src/pages/Landing/index.js
+++ src/pages/Landing/index.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import BackgroundSlider from 'react-background-slider';
 
-const Landing = () => (
-  <div>
-    <h1>Landing</h1>
-  </div>
-);
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+
+const image1 = 'https://source.unsplash.com/E--AUpYXbjM/1600x1000';
+const image2 = 'https://source.unsplash.com/DBGwy7s3QY0/1600x1000';
+const image3 = 'https://source.unsplash.com/7LNatQYMzm4/1600x1000';
+
+const useStyles = makeStyles({
+    root: {
+        position: 'absolute',
+        top: '0',
+        bottom: '0',
+        right: '0',
+        left: '0',
+        display: 'flex'
+    },
+    text: {
+        color: 'white',
+
+    }
+});
+
+function Landing() {
+    const classes = useStyles();
+    return (
+        <Box className={classes.root} p={5}>
+            <BackgroundSlider
+                images={[image1, image2, image3]}
+                duration={5} transition={5} />
+            <Typography variant="h2" component="h1" className={classes.text} >
+                Search, Listen and Download
+            </Typography>
+            <Typography variant="h4" component="h2" className={classes.text} >
+                from the biggest source on the internet.
+            </Typography>
+        </Box>
+    );
+}
 
 export default Landing;
